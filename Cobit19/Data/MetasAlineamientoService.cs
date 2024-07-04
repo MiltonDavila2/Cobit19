4f794822399126e4d285515fc54a440aa5e53e3d
Milton Davila <miltondaviladt@gmail.com>
2024-07-03 23:31:49 -0500

    Cambios dados

--- Cobit19/Data/MetasAlineamientoService.cs
+++ Cobit19/Data/MetasAlineamientoService.cs
@@ -12,12 +12,65 @@ namespace Cobit19.Data
             _dbContextTablas = dbContextTablas;
         }
 
-        public async Task<List<MetasAlineamiento>> GetMetasAlineamientosAsync()
+        public async Task<int> ContarMetasAlineamientosAsync()
         {
-            return await _dbContextTablas.MetasAlineamientos.ToListAsync();
+            return await _dbContextTablas.MetasAlineamientos.CountAsync();
         }
 
-        public async Task<bool> InsertarMetaAlineamiento(MetasAlineamiento metasAlineamiento)
+        public async Task<List<MetasAlineamiento>> GetMetasAlineamientosAsync()
+        {
+
+            var prioridades = new List<string>
+            {
+                "Asegurar el establecimiento y el mantenimiento del marco de gobierno",
+                "Asegurar la obtención de beneficios",
+                "Asegurar la optimización del riesgo",
+                "Asegurar la optimización de los recursos",
+                "Asegurar el compromiso de las partes interesadas",
+                "Gestionar el marco de gestión de I&T",
+                "Gestionar la estrategia",
+                "Gestionar la arquitectura empresarial",
+                "Gestionar la innovación",
+                "Gestionar el portafolio",
+                "Gestionar el presupuesto y los costes",
+                "Gestionar los recursos humanos",
+                "Gestionar las relaciones",
+                "Gestionar los acuerdos de servicio",
+                "Gestionar los proveedores",
+                "Gestionar la calidad",
+                "Gestionar el riesgo",
+                "Gestionar la seguridad",
+                "Gestionar los datos",
+                "Gestionar los programas",
+                "Gestionar la definición de requisitos",
+                "Gestionar la identificación y construcción de soluciones",
+                "Gestionar la disponibilidad",
+                "Gestionar el cambio organizativo",
+                "Gestionar los cambios de TI",
+                "Gestionar la aceptación y la transición de los cambios de TI",
+                "Gestionar el conocimiento",
+                "Gestionar los activos",
+                "Gestionar la configuración",
+                "Gestionar los proyectos",
+                "Gestionar las operaciones",
+                "Gestionar las peticiones y los incidentes de servicio",
+                "Gestionar los problemas",
+                "Gestionar la continuidad",
+                "Gestionar los servicios de seguridad",
+                "Gestionar los controles de procesos de negocio",
+                "Gestionar la monitorización del desempeño y la conformidad",
+                "Gestionar el sistema de control interno",
+                "Gestionar el cumplimiento de los requisitos externos",
+                "Gestionar el aseguramiento"
+            };
+
+
+            var metas = await _dbContextTablas.MetasAlineamientos.ToListAsync();
+
+            return metas.OrderBy(m => prioridades.IndexOf(m.MetaGobierno)).ToList();
+        }
+
+        public async Task<bool> InsertarMetaAlineamientoAsync(MetasAlineamiento metasAlineamiento)
         {
             bool existe= await _dbContextTablas.MetasAlineamientos.AnyAsync(m=>m.MetaGobierno == metasAlineamiento.MetaGobierno);
 
--- Cobit19/Data/MetasEmpresarialesService.cs
+++ Cobit19/Data/MetasEmpresarialesService.cs
@@ -14,8 +14,34 @@ namespace Cobit19.Data
 
         public async Task<List<MetasEmpresariales>> GetMetasEmpresarialesAsync()
         {
-            return await _dbContextTablas.MetasEmpresarialess.ToListAsync();
+            var prioridad = new List<string>
+            {
+                "Cumplimiento y soporte de I&T",
+                "Gestión de riesgo relacionado con I&T",
+                "Beneficios obtenidos del portafolio de inversiones",
+                "Calidad de la información financiera",
+                "Prestación de servicios de I&T",
+                "Agilidad para convertir los requisitos",
+                "Seguridad de la información",
+                "Habilidad y dar soporte a procesos de negocio",
+                "Ejecución de programas dentro del plazo",
+                "Calidad de la información sobre gestión de I&T",
+                "Cumplimiento de I&T con las políticas internas",
+                "Personal competente y motivado",
+                "Conocimiento, experiencia e iniciativas"
+            };
+
+            var metas = await _dbContextTablas.MetasEmpresarialess.ToListAsync();
+
+            return metas.OrderBy(m => prioridad.IndexOf(m.MetaAlineamiento)).ToList();
         }
+
+
+        public async Task<int> ContarMetasEmpresarialesAsync()
+        {
+            return await _dbContextTablas.MetasEmpresarialess.CountAsync();
+        }
+
 
         public async Task<bool> InsertarMetaEmpresarialAsync(MetasEmpresariales metasEmpresariales)
         {
